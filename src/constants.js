31bbe2d8391e1215351bee6c80ff9b52b0a8d27a
tanujsinghchauhan <tanujsinghchauhan38@gmail.com>
2025-04-14 01:25:48 +0530

    components added

---
+++ src/constants.js
@@ -0,0 +1,29 @@
+export const scaleFactor = 4;
+export const dialogueData = {
+  pc: `This is my PC. I work on all my projects on this little piece of technology.
+    I've made a couple of cool things on it sometimes I also use this for digital art.
+    Here is my <a href="https://github.com/tanujsinghchauhan" target="_blank">Github</a>!
+    <a href="https://www.linkedin.com/in/tanuj-singh-chauhan-780ab4289" target="_blank">Linkedin</a>!`,
+  "cs-degree": `This is my Computer Science degree with specialization in Data Science. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also make art time to time. Go to <a href="https://www.deviantart.com/tserenade" target="_blank">my gallery</a>! it's my DeviantArt account
+`,
+  tv: `That's my TV. I've been watching a lot of movies recently, will recommend The Godfather trilogy, Goodfellas, Inglorious Bastards, Jurassic Park(the original one) and Dragon Ball`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. I sometimes read books before bedtime.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/tanujsinghchauhan/Resume/blob/main/tanujresume.pdf" target="_blank">Check it out?</a>
+  Contact me at tanujsinghchauhan38@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of books on my shelves. There is even one in German (I learnt some German in College).
+  I probably only read one of them. I like reading a lot, do you?`,
+  exit: `If you want to exit Tanuj Singh Chauhan's portfolio, just close the tab.`,
+  projector: `This my project storage vault here in these bottles by the wall contain the secret projects that I have worked on`,
+  pro1: `This will lead you to my music player called Nish its a retro music player where you can upload your music or play the sample music present on site in a cool player it needs a lot of work and I will soon add new features to it you can check it out <a href="https://tanujsinghchauhan.github.io/Nish-musicbox/" target="_blank">Here!</a>`,
+  pro2: `This will lead you to my e-commerce site called Craftique it was my first attempt at making a full-stack site, it is focused on helping small artisans earn a living. I tried implementing lot's of cool features in it you can check it out  <a href="https://craftique-delta.vercel.app/" target="_blank">Here!</a>`,
+  pro3: `This will lead you to my club's website I am a part of CodeChef club in my college and I have helped in making components and pages for them you can see them  <a href="https://codechefvitcc.vercel.app/" target="_blank">Here!</a>`,
+  fridge: `I sometimes eat out of this fridge, I also like to cook, what's your favorite dish? Mine is pasta I cook great pasta`,
+  dskills: `This is my development tools & platform vault, I know work with these: GitHub, Figma, Tableau, Firebase, Jupyter-Labs, Postman, Docker `,
+  tskills: `This is my Technical Skills vault, I have the following skills: Full-Stack Development, UI/UX Design, Machine Learning & Data Science, Cloud Computing, Agile Development & CI/CD`,
+  lskills: `This is my Languages and Technolofies vault, I know these languages and technologies: C++, C, Java, Python, SQL, JavaScript, HTML5, Bash, React.js, Next.js, Kaboom.js, Three.js, Tailwind CSS, MongoDB, SQL, Git `,
+  garden: `Having some plants at your house really changes the environment, gardening is one of my hobbies :)`,
+};
