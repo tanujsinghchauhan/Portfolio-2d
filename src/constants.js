630032c1a7c74476a4a1470dd9ce0d30bcb73b5f
tanujsinghchauhan <tanujsinghchauhan38@gmail.com>
2025-09-03 22:17:21 +0530

    fixed links

--- src/constants.js
+++ src/constants.js
@@ -3,8 +3,8 @@ export const dialogueData = {
   pc: `This is my PC. I work on all my projects on this little piece of technology.
     I've made a couple of cool things on it sometimes I also use this for digital art.
     Here is my <a href="https://github.com/tanujsinghchauhan" target="_blank">Github</a>!
-    <a href="https://www.linkedin.com/in/tanuj-singh-chauhan-780ab4289" target="_blank">Linkedin</a>!`,
-  "cs-degree": `This is my Computer Science degree with specialization in Data Science. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+    <a href="https://www.linkedin.com/in/tanuj-singh-chauhan" target="_blank">Linkedin</a>!`,
+  "cs-degree": `This is my Computer Science degree with specialization in Data Science. I hung it on the wall because I'm proud of it. I will hang my other achievements here too`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
   I also make art time to time. Go to <a href="https://www.deviantart.com/tserenade" target="_blank">my gallery</a>! it's my DeviantArt account
 `,
@@ -17,7 +17,7 @@ export const dialogueData = {
   library: `There are a lot of books on my shelves. There is even one in German (I learnt some German in College).
   I probably only read one of them. I like reading a lot, do you?`,
   exit: `If you want to exit Tanuj Singh Chauhan's portfolio, just close the tab.`,
-  projector: `This my project storage vault here in these bottles by the wall contain the secret projects that I have worked on`,
+  projector: `This my project storage vault here in these bottles by the wall contain the secret projects that I have worked on, since its not possible to show all projects here, I suggest you to go directly to my <a href="https://github.com/tanujsinghchauhan" target="_blank">Github</a> still here are some that you can see live.`,
   pro1: `This will lead you to my music player called Nish its a retro music player where you can upload your music or play the sample music present on site in a cool player it needs a lot of work and I will soon add new features to it you can check it out <a href="https://tanujsinghchauhan.github.io/Nish-musicbox/" target="_blank">Here!</a>`,
   pro2: `This will lead you to my e-commerce site called Craftique it was my first attempt at making a full-stack site, it is focused on helping small artisans earn a living. I tried implementing lot's of cool features in it you can check it out  <a href="https://craftique-delta.vercel.app/" target="_blank">Here!</a>`,
   pro3: `This will lead you to my club's website I am a part of CodeChef club in my college and I have helped in making components and pages for them you can see them  <a href="https://codechefvitcc.vercel.app/" target="_blank">Here!</a>`,
