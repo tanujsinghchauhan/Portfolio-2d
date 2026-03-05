2a580b3be88e7a5752d9919fd7a82f35e98d2fd2
Tanuj Singh Chauhan <143076137+tanujsinghchauhan@users.noreply.github.com>
2026-03-05 19:21:57 +0530

    Update constants with new project descriptions and skills

--- src/constants.js
+++ src/constants.js
@@ -6,9 +6,9 @@ export const dialogueData = {
     <a href="https://www.linkedin.com/in/tanuj-singh-chauhan" target="_blank">Linkedin</a>!`,
   "cs-degree": `This is my Computer Science degree with specialization in Data Science. I hung it on the wall because I'm proud of it. I will hang my other achievements here too`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make art time to time. Go to <a href="https://www.deviantart.com/tserenade" target="_blank">my gallery</a>! it's my DeviantArt account
+  I also make art time to time. That's a great way to relax if you ask me." target="_blank">my gallery</a>! it's my DeviantArt account
 `,
-  tv: `That's my TV. I've been watching a lot of movies recently, will recommend The Godfather trilogy, Goodfellas, Inglorious Bastards, Jurassic Park(the original one) and Dragon Ball`,
+  tv: `That's my TV. I've been watching a lot of movies recently, will recommend The Godfather trilogy, Goodfellas, Once Upon a Time in Hollywood, Jurassic Park(the original one) and Dragon Ball`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. I sometimes read books before bedtime.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/tanujsinghchauhan/Resume/blob/main/tanujresume.pdf" target="_blank">Check it out?</a>
   Contact me at tanujsinghchauhan38@gmail.com if you have any interesting job opportunities!`,
@@ -18,12 +18,13 @@ export const dialogueData = {
   I probably only read one of them. I like reading a lot, do you?`,
   exit: `If you want to exit Tanuj Singh Chauhan's portfolio, just close the tab.`,
   projector: `This my project storage vault here in these bottles by the wall contain the secret projects that I have worked on, since its not possible to show all projects here, I suggest you to go directly to my <a href="https://github.com/tanujsinghchauhan" target="_blank">Github</a> still here are some that you can see live.`,
-  pro1: `This will lead you to my music player called Nish its a retro music player where you can upload your music or play the sample music present on site in a cool player it needs a lot of work and I will soon add new features to it you can check it out <a href="https://tanujsinghchauhan.github.io/Nish-musicbox/" target="_blank">Here!</a>`,
+  pro1: `This will lead you to my Peer to Peer Video Call Website called PPVC its a website where you can have a secure peer to peer video call with your buddy. You can share files upto 10MB and even do text messages and screensharing along with it <a href="https://tanujsinghchauhan.github.io/PPVC/" target="_blank">Here!</a>`,
   pro2: `This will lead you to my e-commerce site called Craftique it was my first attempt at making a full-stack site, it is focused on helping small artisans earn a living. I tried implementing lot's of cool features in it you can check it out  <a href="https://craftique-delta.vercel.app/" target="_blank">Here!</a>`,
-  pro3: `This will lead you to my club's website I am a part of CodeChef club in my college and I have helped in making components and pages for them you can see them  <a href="https://codechefvitcc.vercel.app/" target="_blank">Here!</a>`,
+  pro3: `This will lead you to my secure file storage and sharing system called SecureVault, this was my first time when I used AWS to make the entire backend for my projects. You can upload files and they would be stored in a encrypted private bucket on AWS where even if someone got access to the bucket they won't be able to get your data  <a href="https://github.com/tanujsinghchauhan/SecureVault" target="_blank">Here!</a>`,
   fridge: `I sometimes eat out of this fridge, I also like to cook, what's your favorite dish? Mine is pasta I cook great pasta`,
-  dskills: `This is my development tools & platform vault, I know work with these: GitHub, Figma, Tableau, Firebase, Jupyter-Labs, Postman, Docker `,
-  tskills: `This is my Technical Skills vault, I have the following skills: Full-Stack Development, UI/UX Design, Machine Learning & Data Science, Cloud Computing, Agile Development & CI/CD`,
-  lskills: `This is my Languages and Technolofies vault, I know these languages and technologies: C++, C, Java, Python, SQL, JavaScript, HTML5, Bash, React.js, Next.js, Kaboom.js, Three.js, Tailwind CSS, MongoDB, SQL, Git `,
+  dskills: `This is my development tools & platform vault, I know work with these: GitHub, Figma, Tableau, MATLAB, Firebase, JupyterLab, Postman, Docker, AWS, MongoDB, WebRTC `,
+  tskills: `This is my Technical Skills vault, I have the following skills: Full-Stack Web Development, UI/UX Design & Prototyping, Machine Learning & Data Science, Cloud & Serverless
+Architecture, Agile Development & CI/CD Pipelines`,
+  lskills: `This is my Languages and Technolofies vault, I know these languages, frameworks and technologies: C++, C, Java, Python, SQL, R, JavaScript, TypeScript, HTML5, Bash, Go, Git, React, Angular, Next.js, Node.js, Express.js, Gin, Tailwind CSS, Kaboom.js, Three.js`,
   garden: `Having some plants at your house really changes the environment, gardening is one of my hobbies :)`,
 };
